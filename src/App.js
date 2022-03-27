dc8b49a9e5f2ca4f6854bd3624eb3d6a648c5aaf
Allen Tran <allentran08@gmail.com>
2022-03-26 20:01:39 -0700

    more bing chilling clean up!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
 import UnauthedView from './pages/UnauthedView';
 
 function App() {
   return (
     <div className="App">
       <UnauthedView/>
+      <Footer/>
     </div>
+
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Navbar() {
   return (
-    <div>allen's favorite songs</div>
+    <div>unskippables</div>
   )
 }
