666954f650aeeb52c3413da3c135bd36fd65cac9
Allen Tran <allentran08@gmail.com>
2022-03-27 09:46:41 -0700

    added linting and gh actions building

---
+++ src/Pages/AuthedView.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AuthedView() {
+  return (
+    <div>
+      <div>AuthedView</div>
+    </div>
+  );
+}
--- src/pages/AuthedView.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function AuthedView() {
-  return (
-    <div>AuthedView</div>
-  )
-}
